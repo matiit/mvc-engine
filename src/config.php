b4c76ebc8c00339071cfecd35767114ba7cdd882
Kamil Kozaczyński <kamil.ubermade@gmail.com>
2019-01-04 17:40:23 +0100

    Create main config file

---
+++ src/config.php
@@ -0,0 +1,7 @@
+<?php
+
+//Here write your domain with correct protocol (https://example.com/)
+define('HTTP_SERVER', 'http://localhost/mvc-engine/');
+
+//Check for debug mode
+define('DEBUG_MODE', true);
